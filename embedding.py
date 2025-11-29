8e6dcb507227a1ef1ce0d2907d5cc4a33a55878c
JasLian <jliang2024@gmail.com>
2025-11-28 18:33:30 -0600

    added more vector embedding models

--- embedding.py
+++ embedding.py
@@ -16,13 +16,17 @@ conn = psycopg2.connect(
 
 conn.autocommit = False
 
-model = SentenceTransformer("all-MiniLM-L6-v2")
+models = {
+    "mini": SentenceTransformer("all-MiniLM-L6-v2"),
+    "qa": SentenceTransformer("multi-qa-MiniLM-L6-cos-v1"),
+    "mpnet" : SentenceTransformer("sentence-transformers/all-mpnet-base-v2")
+}
 
 BATCH_SIZE = 100
 
 with conn.cursor() as cur:
 
-    cur.execute("SELECT id, text FROM chunks WHERE embedding IS NULL order by id")
+    cur.execute("SELECT id, text FROM chunks WHERE embedding_mini IS NULL order by id")
     rows = cur.fetchall()
 
     for i in range(0, len(rows), BATCH_SIZE):
@@ -30,15 +34,37 @@ with conn.cursor() as cur:
         ids = [r[0] for r in batch]
         texts = [r[1] for r in batch]
 
-        embeddings = model.encode(
-            texts, convert_to_numpy=True, normalize_embeddings=True
+        embedding_mini = models["mini"].encode(
+            texts,
+            convert_to_numpy=True,
+            normalize_embeddings=True
         )
 
-        for chunk_id, emb in zip(ids, embeddings):
-            cur.execute(
-                "UPDATE chunks SET embedding = %s WHERE id = %s",
-                (emb.tolist(), chunk_id)
-            )
+        embedding_qa = models["qa"].encode(
+            texts,
+            convert_to_numpy=True,
+            normalize_embeddings=True
+        )
+
+        embedding_mpnet = models["mpnet"].encode(
+            texts,
+            convert_to_numpy=True,
+            normalize_embeddings=True
+        )
+
+        for idx, chunk_id in enumerate(ids):
+            emb_mini = embedding_mini[idx].tolist()
+            emb_qa = embedding_qa[idx].tolist()
+            emb_bge = embedding_bge[idx].tolist()
+            emb_mpnet = embedding_mpnet[idx].tolist()
+
+            cur.execute("""
+                UPDATE chunks
+                SET embedding_mini = %s,
+                    embedding_qa = %s,
+                    embedding_mpnet = %s
+                WHERE id = %s
+            """, (emb_mini, emb_qa, emb_mpnet, chunk_id))
 
         conn.commit()
         print(f"Processed batch {i}-{i+len(batch)}")
