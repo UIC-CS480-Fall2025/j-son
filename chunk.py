2eff58f11be1065b7f9603abd69050b96315c7d3
JasLian <jliang2024@gmail.com>
2025-11-28 23:00:20 -0600

    chunk txt file as well

--- chunk.py
+++ chunk.py
@@ -17,6 +17,14 @@ def load_jsonl_files(folder_path):
         file_path = os.path.join(folder_path, file)
         yield from load_single_jsonl(file_path)
 
+def load_single_txt(file_path):
+    
+    with open(file_path, "r", encoding="utf-8") as f:
+        for line in f:
+            line = line.strip()
+            if line:
+                yield line
+
 def chunk_text(text, size=150, overlap=50):
     
     words = text.split()
@@ -51,6 +59,21 @@ def chunk_dataset(folder_path, size=150, overlap=50):
 
 def chunk_jsonl_file(file_path, size=150, overlap=50):
 
+    all_chunks = []
+
+    for item in load_single_jsonl(file_path):
+        text = item.get("text", "")
+        if not text:
+            continue
+
+        local_chunks = chunk_text(text, size, overlap)
+        for chunk in local_chunks:
+            if chunk.strip():
+                all_chunks.append(chunk)
+
+    return all_chunks
+
+def chunk_txt_file(file_path, size=150, overlap=50):
     all_chunks = []
 
     for item in load_single_jsonl(file_path):
