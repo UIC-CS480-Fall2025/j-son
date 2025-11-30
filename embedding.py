4125de3e211ddbda536f4b56dc54d8137efccd43
JasLian <jliang2024@gmail.com>
2025-11-30 16:01:49 -0600

    moved model definitions outside of function

--- embedding.py
+++ embedding.py
@@ -1,19 +1,19 @@
+from sentence_transformers import SentenceTransformer
+
+models = {
+    "mini": SentenceTransformer("all-MiniLM-L6-v2"),
+    "qa": SentenceTransformer("multi-qa-MiniLM-L6-cos-v1"),
+    "mpnet" : SentenceTransformer("sentence-transformers/all-mpnet-base-v2")
+}
+
 def create_embedding(text):
-
-    from sentence_transformers import SentenceTransformer
-
-    models = {
-        "mini": SentenceTransformer("all-MiniLM-L6-v2"),
-        "qa": SentenceTransformer("multi-qa-MiniLM-L6-cos-v1"),
-        "mpnet" : SentenceTransformer("sentence-transformers/all-mpnet-base-v2")
-    }
 
     embeddings = {
         name: model.encode(
                 [text],
                 convert_to_numpy=True,
                 normalize_embeddings=True
-            ).tolist()
+            )[0].tolist()
             for name, model in models.items()
     }
 
