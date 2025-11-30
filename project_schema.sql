175ae806952cb0a0fde4f97e5455b5ee53762a75
JasLian <jliang2024@gmail.com>
2025-11-29 21:04:06 -0600

    update syntax

--- project_schema.sql
+++ project_schema.sql
@@ -1,51 +1,50 @@
-Create Table If Not Exists Users (
-    ID Serial Not NULL Primary Key,
-    User_Role varchar(10) Not NULL,
-    User_Name varchar(20) Unique Not NULL,
-    Email varchar(30) Unique Not NULL,
-    User_Password varchar(15) Not NULL,
+CREATE TABLE IF NOT EXISTS public.Users (
+    ID SERIAL NOT NULL PRIMARY KEY,
+    User_Role varchar(10) NOT NULL,
+    User_Name varchar(20) Unique NOT NULL,
+    Email varchar(30) Unique NOT NULL,
+    User_Password varchar(15) NOT NULL,
 
     Check(Email like '_%@_%._%'),
     Check(User_Role in ('Admin', 'Curator', 'EndUser'))
 );
 
-Create Table If Not Exists Document (
-    ID Serial Not NULL Primary Key,
-    Title varchar(80) Not NULL,
-    Doc_Type varchar(20) Not NULL,
-    Added_Time Timestamp Default now() Not NULL,
-    Processed Boolean Default False Not NULL, 
-    Added_By Int Not NULL,
+CREATE TABLE IF NOT EXIST public.Document (
+    ID SERIAL NOT NULL PRIMARY KEY,
+    Title varchar(80) NOT NULL,
+    Doc_Type varchar(20) NOT NULL,
+    Added_Time Timestamp Default now() NOT NULL,
+    Processed BOOLEAN DEFAULT False NOT NULL, 
+    Added_By INT NOT NULL,
 
-    Foreign Key (Added_By) References Users(ID)
+    Foreign Key (Added_By) References Users(ID) ON DELETE CASCADE
 );
 
-Create Table If Not Exists QueryLog (
-    User_ID Int Not NULL,
-    Query_Time Timestamp Default Now() Not NULL,
-    Query_Text Text Not NULL,
+CREATE TABLE IF NOT EXIST public.QueryLog (
+    User_ID INT NOT NULL,
+    Query_Time TIMESTAMP DEFAULT Now() NOT NULL,
+    Query_Text TEXT NOT NULL,
 
-    Foreign Key (User_ID) References Users(ID),
-    Primary Key(User_ID, Query_Time)
+    FOREGIN KEY (User_ID) REFERENCES Users(ID) ON DELETE CASCADE,
+    PRIMARY KEY(User_ID, Query_Time)
 );
-
-Create Table If Not Exists Retrieved_Docs (
-    User_ID Int Not NULL,
-    Query_Time Timestamp Not NULL,
-    Doc_ID Int Not NULL,
+CREATE TABLE IF NOT EXIST public.Retrieved_Docs (
+    User_ID INT NOT NULL,
+    Query_Time TIMESTAMP NOT NULL,
+    Doc_ID INT NOT NULL,
     
-    Foreign Key (User_ID, Query_Time) References QueryLog(User_ID, Query_Time),
-    Foreign Key (Doc_ID) References Document(ID),
-    Primary Key(User_ID, Query_Time, Doc_ID)
+    FOREIGN KEY (User_ID, Query_Time) REFERENCES QueryLog(User_ID, Query_Time),
+    FOREIGN KEY (Doc_ID) REFERENCES Document(ID),
+    PRIMARY KEY(User_ID, Query_Time, Doc_ID)
 );
 
-Create Table If Not Exists Chunk_Embeddings (
-    ID Serial Primary Key,
-    Document_ID Int Not NULL,
-    text Text NOT NULL,
-    embedding_mini vector(384),
-    embedding_qa vector(384),
-    embedding_mpnet vector(768),
+CREATE TABLE IF NOT EXIST public.Chunk_Embeddings (
+    ID SERIAL PRIMARY KEY,
+    Document_ID INT NOT NULL,
+    Chunk_Text TEXT NOT NULL,
+    Embedding_Mini vector(384),
+    Embedding_QA vector(384),
+    Embedding_MPNET vector(768),
 
-    Foreign Key (Document_ID) References Document(ID)
+    FOREIGN KEY (Document_ID) REFERENCES Document(ID) ON DELETE CASCADE
 );
