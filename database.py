58d5188c2dfb69976fea641d674574bff4ac8d52
JasLian <jliang2024@gmail.com>
2025-11-29 21:01:06 -0600

    all functions should be good

--- database.py
+++ database.py
@@ -4,6 +4,7 @@ import os
 import chunk
 import embedding
 from itertools import chain
+from psycopg2.extras import execute_values
 
 load_dotenv()
 
@@ -25,12 +26,12 @@ def initialize_db():
             """)
 
             cur.execute("""
-                Create Table If Not Exists public.Users (
-                    ID Serial Not NULL Primary Key,
-                    User_Role varchar(10) Not NULL,
-                    User_Name varchar(20) Unique Not NULL,
-                    Email varchar(30) Unique Not NULL,
-                    User_Password varchar(15) Not NULL,
+                CREATE TABLE IF NOT EXISTS public.Users (
+                    ID SERIAL NOT NULL PRIMARY KEY,
+                    User_Role varchar(10) NOT NULL,
+                    User_Name varchar(20) Unique NOT NULL,
+                    Email varchar(30) Unique NOT NULL,
+                    User_Password varchar(15) NOT NULL,
 
                     Check(Email like '_%@_%._%'),
                     Check(User_Role in ('Admin', 'Curator', 'EndUser'))
@@ -38,51 +39,51 @@ def initialize_db():
             """)
 
             cur.execute("""
-                Create Table If Not Exists public.Document (
-                    ID Serial Not NULL Primary Key,
-                    Title varchar(80) Not NULL,
-                    Doc_Type varchar(20) Not NULL,
-                    Added_Time Timestamp Default now() Not NULL,
-                    Processed Boolean Default False Not NULL, 
-                    Added_By Int Not NULL,
+                CREATE TABLE IF NOT EXIST public.Document (
+                    ID SERIAL NOT NULL PRIMARY KEY,
+                    Title varchar(80) NOT NULL,
+                    Doc_Type varchar(20) NOT NULL,
+                    Added_Time Timestamp Default now() NOT NULL,
+                    Processed BOOLEAN DEFAULT False NOT NULL, 
+                    Added_By INT NOT NULL,
 
                     Foreign Key (Added_By) References Users(ID) ON DELETE CASCADE
                 );
             """)
 
             cur.execute("""
-                Create Table If Not Exists public.QueryLog (
-                    User_ID Int Not NULL,
-                    Query_Time Timestamp Default Now() Not NULL,
-                    Query_Text Text Not NULL,
+                CREATE TABLE IF NOT EXIST public.QueryLog (
+                    User_ID INT NOT NULL,
+                    Query_Time TIMESTAMP DEFAULT Now() NOT NULL,
+                    Query_Text TEXT NOT NULL,
 
-                    Foreign Key (User_ID) References Users(ID) ON DELETE CASCADE,
-                    Primary Key(User_ID, Query_Time)
+                    FOREGIN KEY (User_ID) REFERENCES Users(ID) ON DELETE CASCADE,
+                    PRIMARY KEY(User_ID, Query_Time)
                 );
             """)
 
             cur.execute("""
-                Create Table If Not Exists public.Retrieved_Docs (
-                    User_ID Int Not NULL,
-                    Query_Time Timestamp Not NULL,
-                    Doc_ID Int Not NULL,
+                CREATE TABLE IF NOT EXIST public.Retrieved_Docs (
+                    User_ID INT NOT NULL,
+                    Query_Time TIMESTAMP NOT NULL,
+                    Doc_ID INT NOT NULL,
                     
-                    Foreign Key (User_ID, Query_Time) References QueryLog(User_ID, Query_Time),
-                    Foreign Key (Doc_ID) References Document(ID),
-                    Primary Key(User_ID, Query_Time, Doc_ID)
+                    FOREIGN KEY (User_ID, Query_Time) REFERENCES QueryLog(User_ID, Query_Time),
+                    FOREIGN KEY (Doc_ID) REFERENCES Document(ID),
+                    PRIMARY KEY(User_ID, Query_Time, Doc_ID)
                 );
             """)
 
             cur.execute("""
-                Create Table If Not Exists public.Chunk_Embeddings (
-                    ID Serial Primary Key,
-                    Document_ID Int Not NULL,
-                    text Text NOT NULL,
-                    embedding_mini vector(384),
-                    embedding_qa vector(384),
-                    embedding_mpnet vector(768),
+                CREATE TABLE IF NOT EXIST public.Chunk_Embeddings (
+                    ID SERIAL PRIMARY KEY,
+                    Document_ID INT NOT NULL,
+                    Chunk_Text TEXT NOT NULL,
+                    Embedding_Mini vector(384),
+                    Embedding_QA vector(384),
+                    Embedding_MPNET vector(768),
 
-                    Foreign Key (Document_ID) References Document(ID) ON DELETE CASCADE
+                    FOREIGN KEY (Document_ID) REFERENCES Document(ID) ON DELETE CASCADE
                 );
             """)
 
@@ -387,5 +388,54 @@ def remove_document(curator, target_doc):
             print("Something went wrong. Please try again.")
             return False
 
-def make_query(user, query):
-    pass
+def make_query(user, query, k):
+    
+    with conn.cursor() as cur:
+        try:
+            cur.execute("""
+                INSERT INTO QueryLog (User_ID, Query_Text)
+                VALUES (%s, %s)
+                RETURNING Query_Time;
+            """, [user, query])
+
+            query_time = cur.fetchone()[0]
+
+            query_embeddings = embedding.create_embedding(query)
+
+            cur.execute("""
+                SELECT Document_ID, Title, Chunk_Text, 
+                    (0.2 * (Embedding_Mini <=> %s::vector) +
+                    0.5 * (Embedding_QA <=> %s::vector) +
+                    0.3 * (Embedding_MPNET <=> %s::vector)
+                    ) AS Distance 
+                FROM Chunk_Embeddings
+                JOIN Document ON Document.ID = Chunk_Embeddings.Document_ID
+                ORDER BY Distance
+                LIMIT %s;
+            """, [query_embeddings["mini"], query_embeddings["qa"], query_embeddings["mpnet"], k])
+
+            top_k = cur.fetchall()
+
+            retrieved_docs_ids = []
+            retrieved_doc_titles = []
+            retrieved_chunks = []
+            for r in top_k:
+                retrieved_docs_ids.append(r[0])
+                retrieved_doc_titles.append(r[1])
+                retrieved_chunks.append(r[2])
+
+            values = [(user, query_time, doc_id) for doc_id in retrieved_docs_ids]
+            execute_values(cur, """
+                INSERT INTO Retrieved_Docs (User_ID, Query_Time, Doc_ID)
+                VALUES %s;
+            """, values)
+            
+            conn.commit()
+            return retrieved_doc_titles, retrieved_chunks
+        
+        except Exception as e:
+            conn.rollback()
+            print("Something went wrong. " + str(e) + "\n Please try again.")
+            return []
+    
+
